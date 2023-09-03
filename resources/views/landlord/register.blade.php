5c4236ed617ff720694dd9617ef18b86d93ea5fd
fennereduardo <fennereduardo@gmail.com>
2023-09-03 18:32:25 -0500

    :fire: Adding Breeze to the FrontEnd

--- resources/views/landlord/register.blade.php
+++ resources/views/landlord/register.blade.php
@@ -1,4 +1,4 @@
-{{-- <x-guest-layout> --}}
+<x-guest-layout>
     <form action="{{ route('landlord.register') }}" method="post">
         <div class="flex flex-col items-center justify-center min-h-screen space-y-4">
             <h1 class="py-4 text-4xl font-bold text-indigo-700"></h1>
@@ -10,10 +10,10 @@
                 <input type="text" name="domain" id="" placeholder="Domain">
             </div>
             <div>
-                <button type="submit">Crear</button>
+                <button class="px-8 py-1.5 text-white bg-indigo-500 rounded" type="submit">Crear</button>
             </div>
 
         </div>
 
     </form>
-{{-- </x-guest-layout> --}}
+</x-guest-layout>
